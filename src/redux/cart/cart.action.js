08c675c6ab757f9aa0e36817c7c82af7ee42b6df
Akshat <akvs0707@gmail.com>
2020-12-20 23:22:39 +0530

    Updated cart utils to group data in cart reducer

--- src/redux/cart/cart.action.js
+++ src/redux/cart/cart.action.js
@@ -1,6 +1,11 @@
 import CartActionTypes from './cart.types';
 
-
+// eslint-disable-next-line
 export const toggleCartHidden =()=> ({
     type: CartActionTypes.TOGGLE_CART_HIDDEN
 });
+
+export const addItem = (item) => ({
+    type: CartActionTypes.ADD_ITEM,
+    payload: item
+})
