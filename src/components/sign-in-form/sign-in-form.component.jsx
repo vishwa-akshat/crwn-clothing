527f63175b306d068bcc6aadd903690b291f8270
vishwa-akshat <akvs0707@gmail.com>
2023-11-19 22:20:21 +0530

    Completed Authentication page and its features

---
+++ src/components/sign-in-form/sign-in-form.component.jsx
@@ -0,0 +1,98 @@
+import { useState } from "react";
+import {
+    createUserDocumentFromAuth,
+    signInWithGooglePopup,
+    signInAuthUserWithEmailAndPassword,
+} from "../../utils/firebase/firebase.utils";
+import FormInput from "../form-input/form-input.component";
+import Button from "../button/button.component";
+
+import "./sign-in-form.styles.scss";
+
+const defaultFormField = {
+    email: "",
+    password: "",
+};
+
+export default function SignInForm() {
+    const [formFields, setFormFields] = useState(defaultFormField);
+
+    const { email, password } = formFields;
+
+    const resetFormFields = () => {
+        setFormFields(defaultFormField);
+    };
+
+    const handleChange = (event) => {
+        const {
+            target: { name, value },
+        } = event;
+
+        setFormFields((formFields) => ({ ...formFields, [name]: value }));
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+
+        try {
+            const response = await signInAuthUserWithEmailAndPassword(
+                email,
+                password
+            );
+            console.log(response);
+            resetFormFields();
+        } catch (error) {
+            switch (error.code) {
+                case "auth/wrong-password":
+                    alert("incorrect password for email");
+                    break;
+                case "auth/user-not-found":
+                    alert("no user associated with this email");
+                    break;
+                default:
+                    console.log(error);
+            }
+        }
+    };
+
+    const signInWithGoogle = async () => {
+        const { user } = await signInWithGooglePopup();
+        await createUserDocumentFromAuth(user);
+    };
+
+    return (
+        <div className="sign-up-container">
+            <h2>Already have an account</h2>
+            <span>Sign in with your email and password</span>
+            <form onSubmit={handleSubmit}>
+                <FormInput
+                    label="Email"
+                    onChange={handleChange}
+                    value={email}
+                    name="email"
+                    type="email"
+                    required
+                />
+
+                <FormInput
+                    label="Password"
+                    onChange={handleChange}
+                    value={password}
+                    name="password"
+                    type="password"
+                    required
+                />
+                <div className="buttons-container">
+                    <Button type="submit">Sign In</Button>
+                    <Button
+                        type="button"
+                        buttonType="google"
+                        onClick={signInWithGoogle}
+                    >
+                        Google Sign In
+                    </Button>
+                </div>
+            </form>
+        </div>
+    );
+}
---
+++ src/routes/authentication/authentication.component.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+import SignUpForm from "../../components/sign-up-form/sign-up-form.component";
+import SignInForm from "../../components/sign-in-form/sign-in-form.component";
+
+import "./authentication.styles.scss";
+
+export default function Authentication() {
+    return (
+        <div className="authentication-container">
+            <SignInForm />
+            <SignUpForm />
+        </div>
+    );
+}
