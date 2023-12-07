148af144cd6512447be09caa883321c6d111b313
vishwa-akshat <akvs0707@gmail.com>
2023-12-07 15:06:07 +0530

    Adding cart context and cart icon and dropdown

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -6,11 +6,15 @@ import { ReactComponent as CrwnLogo } from "../../assets/crown.svg";
 import { signOutUser } from "../../utils/firebase/firebase.utils";
 
 import { UserContext } from "../../contexts/user.context";
+import { CartContext } from "../../contexts/cart.context";
 
 import "./navigation.styles.scss";
+import CartIcon from "../../components/cart-icon/cart-icon.component";
+import CartDropdown from "../../components/cart-dropdown/cart-dropdown.component";
 
 const Navigation = () => {
     const { currentUser } = useContext(UserContext);
+    const { isCartOpen } = useContext(CartContext);
 
     return (
         <Fragment>
@@ -22,6 +26,7 @@ const Navigation = () => {
                     <Link className="nav-link" to="/shop">
                         SHOP
                     </Link>
+
                     {currentUser ? (
                         <span className="nav-link" onClick={signOutUser}>
                             SIGN OUT
@@ -31,7 +36,9 @@ const Navigation = () => {
                             SIGN IN
                         </Link>
                     )}
+                    <CartIcon />
                 </div>
+                {isCartOpen && <CartDropdown />}
             </div>
             <Outlet />
         </Fragment>
---
+++ src/routes/shop/shop.component.jsx
@@ -0,0 +1,19 @@
+import React, { useContext } from "react";
+
+import ProductCard from "../../components/product-card/product-card.component";
+
+import { ProductsContext } from "../../contexts/products.context";
+
+import "./shop.styles.scss";
+
+export default function Shop() {
+    const { products } = useContext(ProductsContext);
+
+    return (
+        <div className="products-container">
+            {products.map((product) => (
+                <ProductCard key={product.id} product={product} />
+            ))}
+        </div>
+    );
+}
