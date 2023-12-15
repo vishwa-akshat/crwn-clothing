aa9cd89f7d20e2e59e73137948122c752d12736a
vishwa-akshat <akvs0707@gmail.com>
2023-12-15 17:23:45 +0530

    Added stripe for payment

--- src/index.js
+++ src/index.js
@@ -2,9 +2,12 @@ import React from "react";
 import { render } from "react-dom";
 import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
+import { Elements } from "@stripe/react-stripe-js";
 
 import App from "./App";
 import { store } from "./store/store";
+
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import "./index.scss";
 
@@ -14,7 +17,9 @@ render(
     <React.StrictMode>
         <Provider store={store}>
             <BrowserRouter>
-                <App />
+                <Elements stripe={stripePromise}>
+                    <App />
+                </Elements>
             </BrowserRouter>
         </Provider>
     </React.StrictMode>,
